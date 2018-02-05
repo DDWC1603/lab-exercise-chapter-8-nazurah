4713a46444144edffda7339ab90ddd3ca1cfccc0
NazurahKamal <33883971+NazurahKamal@users.noreply.github.com>
2018-02-05 21:22:11 +0800

    updated

---
+++ log.project.classes.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Log
+{
+	public:
+		enum level{error=0, warning=1, info=2};
+private:
+	int m_LogLevel=info;
+
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel=level;
+	}
+	void Error(const char* message)
+	{
+		if(m_LogLevel>=error)
+			cout<<"[ERROR]: "<<message <<endl;
+			
+	}
+	void Warn(const char* message)
+	{
+		if(m_LogLevel>=warning)
+			cout<<"[WARNING]: "<<message <<endl;
+	}
+	void Info(const char* message)
+	{
+		if(m_LogLevel>=info)
+			cout<<"[INFO]: "<<message <<endl;
+	}
+	
+};
+
+int main()
+
+{
+Log oLog;
+oLog.info;
+oLog.Info("HELLO!!!");
+	
+std::cin.get();
+	
+}
